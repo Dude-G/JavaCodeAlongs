fd8bda96a474ff9775a0fd08725a75a29dd9d392
𝐒𝖙𝖆𝖓-𝖑𝖊𝖊 <stanchukwu389@gmail.com>
2026-02-10 11:43:58 +0100

    first commit

---
+++ untitled/src/Codechallenge1.java
@@ -0,0 +1,9 @@
+public class Codechallenge1 {
+    public static void main(String[] args) {
+    int itemPrice = 3000;
+    int shippingCost = 2500;
+
+    int sum = itemPrice + shippingCost;
+    System.out.println(sum);
+    }
+}
---
+++ untitled/src/Codechallenge2.java
@@ -0,0 +1,8 @@
+public class Codechallenge2 {
+   public static void main(String[] args) {
+     String message = "Java is Fun";
+        System.out.println(message);
+
+
+    }
+}
