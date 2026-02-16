9e3d55939a4041ff760e992167e18791e7fe3d7b
𝐒𝖙𝖆𝖓-𝖑𝖊𝖊 <stanchukwu389@gmail.com>
2026-02-16 11:34:14 +0100

    added some files

---
+++ untitled/src/Codechallenge3.java
@@ -0,0 +1,9 @@
+public class Codechallenge3 {
+    static void welcomeMessage(){
+        System.out.println("Welcome to Java!");
+    }
+
+    static void main(String[] args) {
+        welcomeMessage();
+    }
+}
---
+++ untitled/src/Codechallenge4.java
@@ -0,0 +1,9 @@
+public class Codechallenge4 {
+    static void welcomeMessage(String name) {
+        System.out.println("Welcome to Java, " + name);
+    }
+
+    static void main(String[] args) {
+        welcomeMessage("Stan");
+    }
+}
