23ac5702a8ae6badc524a5edabf594d366a75d95
𝐒𝖙𝖆𝖓-𝖑𝖊𝖊 <stanchukwu389@gmail.com>
2026-02-18 10:55:05 +0100

    added some files

---
+++ untitled/src/Codechallenge5.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+      int age = 25;
+      if (age >=18){
+          System.out.println("Allowed");
+      }
+      else{
+         System.out.println("Not allowed");
+      }
+    }
+}
