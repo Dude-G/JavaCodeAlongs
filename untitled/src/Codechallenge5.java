31cd5d4f34eb54a19348d5f09f6b3a6fc927d43b
𝐒𝖙𝖆𝖓-𝖑𝖊𝖊 <stanchukwu389@gmail.com>
2026-02-23 09:31:40 +0100

    added some files

--- untitled/src/Codechallenge5.java
+++ untitled/src/Codechallenge5.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Codechallenge5 {
     public static void main(String[] args) {
       int age = 25;
       if (age >=18){
--- untitled/src/Codechallenge6.java
+++ untitled/src/Codechallenge6.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Codechallenge6 {
     public static void main(String[] args) {
         int choice = 1;
 
