0c05406bac69a8b853c0fe4b8e0c62d237eefc72
𝐒𝖙𝖆𝖓-𝖑𝖊𝖊 <stanchukwu389@gmail.com>
2026-02-23 11:41:07 +0100

    added some files

---
+++ untitled/src/Codechallenge7.java
@@ -0,0 +1,10 @@
+public class Codechallenge7 {
+    String city = "London";
+    int population = 9000000;
+
+    public static void main(String[] args) {
+        Codechallenge7 myObj = new Codechallenge7();
+        System.out.println(myObj.city);
+        System.out.println(myObj.population);
+    }
+}
